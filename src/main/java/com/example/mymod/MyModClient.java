f80c52c8cf7e369a00b6d90547d40a86e0969251
zzhbro4ka <166872220+zzhbro4ka@users.noreply.github.com>
2026-06-30 02:19:18 +0300

    Update MyModClient.java

--- src/main/java/com/example/mymod/MyModClient.java
+++ src/main/java/com/example/mymod/MyModClient.java
@@ -14,15 +14,13 @@ public class MyModClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        // Регистрируем клавишу Right Shift
         openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.mymod.open_menu",       // translation key
+                "key.mymod.open_menu",
                 InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_RIGHT_SHIFT,   // Right Shift
-                "category.mymod.general"     // категория в настройках
+                GLFW.GLFW_KEY_RIGHT_SHIFT,
+                "category.mymod.general"
         ));
 
-        // Слушаем нажатие каждый тик
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openMenuKey.wasPressed()) {
                 if (client.player != null) {
